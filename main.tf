e2e84097d146c2bd1198a2ff18d4957f2d98b1a2
rawalnaman <130594665+rawalnaman@users.noreply.github.com>
2025-01-20 13:01:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,8 +46,8 @@ resource "aws_instance" "web" {
 resource "aws_security_group" "web-sg" {
   name = "web-sg"
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
