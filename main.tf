04014b4cede38a152e5d2ae2ed76e41d0c61a78c
rawalnaman <130594665+rawalnaman@users.noreply.github.com>
2025-02-02 09:28:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -134,12 +134,13 @@ resource "google_compute_instance" "default" {
   }
 
   metadata_startup_script = "echo hi > /test.txt"
-
+/*
   service_account {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     email  = google_service_account.default.email
     scopes = ["cloud-platform"]
   }
+*/
 }
 
 
